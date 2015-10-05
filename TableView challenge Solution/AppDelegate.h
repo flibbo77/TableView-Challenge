bf9357d741bd5a72b65c075bc03b866a3e8b69df
Philipp Roehl <philipp.roehl@trendantenna-imac.local>
2015-10-05 12:53:17 +0200

    Initial Commit

---
+++ TableView challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView challenge Solution
+//
+//  Created by Philipp Roehl on 05.10.15.
+//  Copyright © 2015 Continental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
